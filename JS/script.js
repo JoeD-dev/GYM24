75e85d693dcc3c1bdfb75a26d50800c67794a0bd
JoeD-dev <jd-mail@tutamail.com>
2025-08-06 19:46:56 +0300

    feat(script): add script for slider inside about us section

--- JS/script.js
+++ JS/script.js
@@ -16,28 +16,50 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         
     });
+    let aboutSlider = null;
     let mobileSwiper = null;
     const BREAKPOINT = 744;
     function handleMobileSwiper() {
         if (mobileSwiper === null && window.innerWidth <= BREAKPOINT) {
-        mobileSwiper = new Swiper('.swiper', {
-        loop: true,
-        watchSlidesProdress: true,
-        watchSlidesVisibility: true,
-        slidesPerView: 'auto',
-        spaceBetween: 12,
-        grabCursor: true,
-        centeredSlides: true,
-        initialSlide: 1,
-        });
-        setTimeout(() => {
-            swiper.update();
-        
-        }, 100);
+            mobileSwiper = new Swiper('.membership', {
+                loop: true,
+                watchSlidesProdress: true,
+                watchSlidesVisibility: true,
+                slidesPerView: 'auto',
+                spaceBetween: 12,
+                grabCursor: true,
+                centeredSlides: true,
+                initialSlide: 1,
+            });
+            if(aboutSlider !== null) {
+                aboutSlider.destroy(true, true);
+            }
+            aboutSlider = new Swiper ('.about-slider', {
+                loop: true,
+                pagination: false,
+                spaceBetween: 12,
+            })
         }else if(mobileSwiper !== null && window.innerWidth > BREAKPOINT)  { 
             mobileSwiper.destroy(true, true);
             mobileSwiper = null;
-        };
+            
+        }else if(window.innerWidth > BREAKPOINT) {
+            if(aboutSlider !== null) {
+                aboutSlider.destroy(true, true);
+            }
+            aboutSlider = new Swiper ('.about-slider', {
+                loop: true,
+                spaceBetween: 12,
+                slidesPerView: 1,
+                pagination: {
+                    el: '.pagination',
+                },
+                navigation: {
+                    nextEl: '.next',
+                    prevEl: '.prev'
+                }
+            })
+        }
     };
     function debounce(func, delay) {
         let timeout;
@@ -48,5 +70,6 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     window.addEventListener('load', handleMobileSwiper);
     window.addEventListener('resize', handleMobileSwiper, 200);
+    
 });
 
